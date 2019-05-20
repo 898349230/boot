44b33b874f4576cfa4a7c78f4d8654030a91d112
sun <898349230@qq.com>
2019-05-20 16:05:03 +0800

    增加 redis-starter 简单测试

--- src/main/java/com/ab/controller/ApiController.java
+++ src/main/java/com/ab/controller/ApiController.java
@@ -1,6 +1,7 @@
 package com.ab.controller;
 
 import com.ab.rabbitmq.RabbitMQSenderService;
+import com.ab.redis.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,9 @@ public class ApiController {
 
     @Autowired
     private RabbitMQSenderService rabbitMQSenderService;
+    @Autowired
+    private RedisService redisService;
+
     /**
      * 测试 发送rabbitMQ消息
      * @param message
@@ -25,4 +29,31 @@ public class ApiController {
         rabbitMQSenderService.sendMessage(message);
         return "success";
     }
+
+    /**
+     * 测试 redis
+     * @param key
+     * @param value
+     * @return
+     */
+    @GetMapping(value = "/redisSet")
+    @ResponseBody
+    public String redisSet(@RequestParam(value = "key")String key, @RequestParam(value = "value")String value){
+        redisService.setString(key, value);
+        return "success";
+    }
+
+    /**
+     * 测试 redis
+     * @param key
+     * @return
+     */
+    @GetMapping(value = "/redisGet")
+    @ResponseBody
+    public String redisGet(@RequestParam(value = "key")String key){
+        String str = redisService.getString(key);
+        return str;
+    }
+
+
 }
---
+++ src/main/java/com/ab/redis/RedisService.java
@@ -0,0 +1,25 @@
+package com.ab.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisService {
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    public String getString(String key){
+
+        ValueOperations ops = redisTemplate.opsForValue();
+        return (String) ops.get(key);
+    }
+
+    public void setString(String key, String value){
+
+        ValueOperations ops = redisTemplate.opsForValue();
+        ops.set(key, value);
+    }
+}
