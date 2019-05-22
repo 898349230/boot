c85068092613fc4c20737e95bcb2b340dbcad96f
sun <898349230@qq.com>
2019-05-22 13:45:53 +0800

    增加事务注解

--- src/main/java/com/ab/service/HelloService.java
+++ src/main/java/com/ab/service/HelloService.java
@@ -3,10 +3,12 @@ package com.ab.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
-import javax.swing.*;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.REPEATABLE_READ)
 public class HelloService {
 
     private static Logger logger = LoggerFactory.getLogger(HelloService.class);
